6aaf54b4c8731c2e997741a11f1b0e1afc42e26d
mr.one.id <101521147+onepunya@users.noreply.github.com>
2024-03-31 22:33:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const axios = require('axios');
+const { Prodia } = require("prodia.js");
+const prodia = new Prodia("cdffa14f-c399-42b6-af90-ad25be1f8ba6"); // API KEY HERE
 global.creator = 'Mr.one | github/onepunya'
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,78 +13,34 @@ app.set("json spaces", 2);
 // Middleware untuk CORS
 app.use(cors());
 
-// Fungsi untuk ragBot
-async function ragBot(message) {
-  try {
-    const response = await axios.post('https://ragbot-starter.vercel.app/api/chat', {
-      messages: [{ role: 'user', content: message }],
-      useRag: true,
-      llm: 'gpt-3.5-turbo',
-      similarityMetric: 'cosine'
-    });
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-}
-
-// Fungsi untuk degreeGuru
-async function degreeGuru(message, prompt) {
-  try {
-    const response = await axios.post('https://degreeguru.vercel.app/api/guru', {
-      messages: [
-        { role: 'user', content: message }
-      ]
-    });
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-}
-
-// Fungsi untuk pinecone
-async function pinecone(message) {
-  try {
-    const response = await axios.post('https://pinecone-vercel-example.vercel.app/api/chat', {
-      messages: [{ role: 'user', content: message }]
-    });
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-}
-
-// Fungsi untuk smartContract
-async function smartContract(message) {
-  try {
-    const response = await axios.post("https://smart-contract-gpt.vercel.app/api/chat", {
-      messages: [{ content: message, role: "user" }]
-    });
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-}
-
-async function blackboxAIChat(message) {
-  try {
-    const response = await axios.post('https://www.blackbox.ai/api/chat', {
-      messages: [{ id: null, content: message, role: 'user' }],
-      id: null,
-      previewToken: null,
-      userId: null,
-      codeModelMode: true,
-      agentMode: {},
-      trendingAgentMode: {},
-      isMicMode: false,
-      isChromeExt: false,
-      githubToken: null
-    });
-
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+//fungsi untuk imagine
+async function imagine(prompt) {
+    try {
+        const generate = await prodia.generateImage({
+            prompt: prompt,
+            model: "majicmixRealistic_v4.safetensors [29d0de58]",
+            negative_prompt: "BadDream, (UnrealisticDream:1.3)",
+            sampler: "DPM++ SDE Karras",
+            cfg_scale: 9,
+            steps: 30,
+            aspect_ratio: "portrait"
+        });
+        
+        let toy = `https://images.prodia.xyz/${generate.job}.png`;
+        
+        return new Promise((resolve, reject) => {
+            setTimeout(async () => {
+                try {
+                    let data = await axios.get(`https://aemt.me/remini?url=${toy}&resolusi=2`);
+                    resolve(data.data.url);
+                } catch (error) {
+                    reject(error);
+                }
+            }, 20000);
+        });
+    } catch (e) {
+        throw e;
+    }
 }
 
 // Endpoint untuk servis dokumen HTML
@@ -93,14 +51,14 @@ app.get('/doc', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Endpoint untuk ragBot
-app.get('/api/ragbot', async (req, res) => {
+// Endpoint untuk imagine
+app.get('/api/imagine', async (req, res) => {
   try {
-    const message = req.query.message;
+    const message = req.query.prompt;
     if (!message) {
-      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
+      return res.status(400).json({ error: 'Parameter "prompt" tidak ditemukan' });
     }
-    const response = await ragBot(message);
+    const response = await imagine(prompt);
     res.status(200).json({
       status: 200,
       creator: global.creator,
@@ -111,78 +69,7 @@ app.get('/api/ragbot', async (req, res) => {
   }
 });
 
-// Endpoint untuk degreeGuru
-app.get('/api/degreeguru', async (req, res) => {
-  try {
-    const { message }= req.query;
-    if (!message) {
-      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
-    }
-    const response = await degreeGuru(message);
-    res.status(200).json({
-      status: 200,
-      creator: global.creator,
-      data: { response }
-    });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-// Endpoint untuk pinecone
-app.get('/api/pinecone', async (req, res) => {
-  try {
-    const message = req.query.message;
-    if (!message) {
-      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
-    }
-    const response = await pinecone(message);
-    res.status(200).json({
-      status: 200,
-      creator: global.creator,
-      data: { response }
-    });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-// Endpoint untuk smartContract
-app.get('/api/smartcontract', async (req, res) => {
-  try {
-    const message = req.query.message;
-    if (!message) {
-      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
-    }
-    const response = await smartContract(message);
-    res.status(200).json({
-      status: 200,
-      creator: global.creator,
-      data: { response }
-    });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-// Endpoint untuk blackboxAIChat
-app.get('/api/blackboxAIChat', async (req, res) => {
-  try {
-    const message = req.query.message;
-    if (!message) {
-      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
-    }
-    const response = await blackboxAIChat(message);
-    res.status(200).json({
-      status: 200,
-      creator: global.creator,
-      data: { response }
-    });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
+/
 // Handle 404 error
 app.use((req, res, next) => {
   res.status(404).send("Sorry can't find that!");
