8b7674a38b5a3ac3b82ae6752998d9c25dd666f3
mr.one.id <101521147+onepunya@users.noreply.github.com>
2024-04-01 18:17:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const jimp = require('jimp');
+const fetch = require('node-fetch');
 const axios = require('axios');
 const { Prodia } = require("prodia.js");
 const prodiakey = "cdffa14f-c399-42b6-af90-ad25be1f8ba6"// API KEY HERE
+const googlekey = "AIzaSyAY8DjFZHICDZ-TeHNN6lnEFoB-qczmXxE"
 global.creator = 'Mr.one | github/onepunya'
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,6 +15,78 @@ app.set("json spaces", 2);
 
 // Middleware untuk CORS
 app.use(cors());
+//fungsi buffer 
+async function bufferlah(hm) {
+const imageUrl = hm;
+const imagePath = 'gambar.jpg';
+
+const response= await axios({
+  method: 'get',
+  url: imageUrl,
+  responseType: 'arraybuffer'
+})
+  const buffer = Buffer.from(response.data, 'binary');
+  return buffer;   
+}
+//fungsi resize
+// Memuat gambar dari file
+async function Resize(buffer) {
+    var oyy = await jimp.read(buffer);
+    var kiyomasa = await oyy.resize(512, 512).getBufferAsync(jimp.MIME_JPEG)
+    return kiyomasa
+}
+//fungsi gemini
+async function ask(inputText) {
+  // For text-only input, use the gemini-pro model
+const url = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=' + googlekey;
+const headers = {
+    'Content-Type': 'application/json'
+};
+const data = {
+    contents: [{
+        parts: [{
+            text: inputText
+        }]
+    }]
+};
+
+const response = await axios.post(url, data, { headers })
+        console.log(response.data.candidates[0].content.parts[0].text);
+    return response;
+    }
+    
+// image input gemini vision
+async function askImage(inputTextt, inputImage) {
+const bufer = await bufferlah(inputImage) 
+const buff = await Resize(bufer)
+    const requestBody = {
+        "contents": [
+            {
+                "parts": [
+                    {"text": inputTextt},
+                    {
+                        "inline_data": {
+                            "mime_type": "image/jpeg",
+                            "data": buff.toString('base64')
+                        }
+                    }
+                ]
+            }
+        ]
+    };
+    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-pro-vision:generateContent?key=${googlekey}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestBody)
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+    return data
+}
 
 //fungsi untuk prodia
 async function pprodia(message) {
@@ -35,14 +110,14 @@ const prodia = new Prodia(prodiakey);
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'interface.html'));
 });
-app.get('/doc', (req, res) => {
+app.get('/docs', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 app.get('/end', (req, res) => {
   res.sendFile(path.join(__dirname, 'endpoint.html'));
 });
 
-// Endpoint untuk imagine
+// Endpoint untuk prodia
 app.get('/api/prodia', async (req, res) => {
   try {
     const message = req.query.prompt;
@@ -62,7 +137,48 @@ app.get('/api/prodia', async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
+//endpoint gemini
+app.get('/api/gemini', async (req, res) => {
+  try {
+    const message = req.query.text;
+    if (!message) {
+      return res.status(400).json({ error: 'Parameter "text" tidak ditemukan' });
+    }
+    const data = await ask(message);
+    res.status(200).json({
+      status: 200,
+      creator: global.creator,
+      data, 
 
+    });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+// endpoint gemini-image
+app.get('/api/gemini-vision', async (req, res) => {
+  try {
+    const gambar = req.query.url
+    const message = req.query.text;
+    if (!message) {
+      return res.status(400).json({ error: 'Parameter "text" tidak ditemukan' });
+    }
+    if (!gambar) {
+      return res.status(400).json({ error: 'Parameter "url" tidak ditemukan pastikan url gambar ada pada endpoint' });
+ 
+    const data = await ask(message, gambar);
+    res.status(200).json({
+      status: 200,
+      creator: global.creator,
+      data, 
+
+    });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 // Handle 404 error
 app.use((req, res, next) => {
   res.status(404).send("Sorry can't find that!");
